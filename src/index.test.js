c9214d1807ddccd0d8a5dfba91e30fd37b392c9f
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-02-20 12:56:57 +0100

    No events and bounds

--- src/index.test.js
+++ src/index.test.js
@@ -13,6 +13,27 @@ describe("timeline", () => {
 
   test("returns matching events", () => {
     expect(subject(0, events)).toEqual([{ id: "1", start: 0, end: 1 }]);
+    expect(subject(1.5, events)).toEqual([{ id: "2", start: 1, end: 2 }]);
+  });
+
+  test("returns no events that are out of range", () => {
+    expect(subject(-10, events)).toEqual([]);
+    expect(subject(10, events)).toEqual([]);
+  });
+
+  const overlapping = [
+    { id: "1", start: 0, end: 1.5 },
+    { id: "2", start: 0.5, end: 2 }
+  ];
+
+  test("events can overlap in time", () => {
+    expect(subject(1.1, overlapping)).toEqual([
+      { id: "1", start: 0, end: 1.5 },
+      { id: "2", start: 0.5, end: 2 }
+    ]);
+    expect(subject(1.8, overlapping)).toEqual([
+      { id: "2", start: 0.5, end: 2 }
+    ]);
   });
 
   test("matching event should be a clone, not the actual object", () => {
