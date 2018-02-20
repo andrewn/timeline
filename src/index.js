b23b7b418b118607a1b1ff9a48ac6c76c174f83a
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-02-20 12:53:18 +0100

    Basic event support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-export default (currentTime = 0, events = []) => [];
+const isValid = event =>
+  event != null &&
+  typeof event.start === "number" &&
+  typeof event.end === "number";
+
+export default (currentTime = 0, events = []) => {
+  const cloned = JSON.parse(JSON.stringify(events));
+
+  return cloned.reduce((matching, event) => {
+    if (!isValid(event)) {
+      throw new Error(`start and end must be a number`);
+    }
+
+    if (event.start <= currentTime && event.end > currentTime) {
+      matching.push(event);
+    }
+
+    return matching;
+  }, []);
+};
--- src/index.test.js
+++ src/index.test.js
@@ -5,7 +5,21 @@ describe("timeline", () => {
     expect(typeof subject).toBe("function");
   });
 
-  test("returns matching events ", () => {
+  test("returns no events by default", () => {
     expect(subject(0, [])).toEqual([]);
+  });
+
+  const events = [{ id: "1", start: 0, end: 1 }, { id: "2", start: 1, end: 2 }];
+
+  test("returns matching events", () => {
+    expect(subject(0, events)).toEqual([{ id: "1", start: 0, end: 1 }]);
+  });
+
+  test("matching event should be a clone, not the actual object", () => {
+    expect(subject(0, events)[0]).not.toBe(events[0]);
+  });
+
+  test("start and end must be numbers", () => {
+    expect(() => subject(0, [{ id: "1", start: "0", end: "1.234" }])).toThrow();
   });
 });
